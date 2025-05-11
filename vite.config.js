b5c8e8e5d68dbf287af0d6b1eacb531982626c21
rajivsharma25 <rajivsharma93056@gmail.com>
2025-05-12 02:41:17 +0530

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/College-Pravesh-Website/', // <-- REPO NAME here
+  base: './',
   plugins: [react()],
 });
